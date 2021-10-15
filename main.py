06c58d324c5612f50cd93c59bdebfff81aeac638
1azyPotato <42973475+A-Juicy-Potato@users.noreply.github.com>
2021-10-15 10:08:29 +0000

    bug fix

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ pupilname = input("ENTER FULL NAME: ")
 validforms = ["1C","1F","1H","1M","1O","1S","2C","2F","2H",
 "2M","2O","2S","3C","3F","3H","3M","3O","3S"]
 
-validinterest = ["Python","Scratch"]
+validinterest = ["python","scratch"]
 
-validskill = ["Pioneer", "Explorer", "Scout", "Expert"]
+validskill = ["pioneer", "explorer", "scout", "expert"]
 
 form = ""
 while form not in validforms:
-  form = input("ENTER FORM: ")
+  form = input("ENTER FORM: ").upper()
 
 interest = ""
 while interest not in validinterest:
-  interest = input("PYTHON OR SCRATCH: ")
+  interest = input("PYTHON OR SCRATCH: ").lower()
 
 skill = ""
 while skill not in validskill:
-  skill = input("ENTER SKILL LEVEL: PIONEER , EXPLORER , SCOUT , EXPERT: ")
+  skill = input("ENTER SKILL LEVEL: PIONEER , EXPLORER , SCOUT , EXPERT: ").lower()
 
 attendancedate = input("ATTENDANCE DATE: ")
 
